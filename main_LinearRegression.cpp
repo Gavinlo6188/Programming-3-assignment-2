61a2de76fb403e542aaa53353f518f90e9856cde
Lightfoot (Ammar) <161982421+Lightfoot7@users.noreply.github.com>
2026-05-24 23:46:07 +0100

    Update run command comment in main_LinearRegression.cpp

--- main_LinearRegression.cpp
+++ main_LinearRegression.cpp
@@ -1,4 +1,4 @@
-//To run the code use "g++ -Iinclude main_LinearRegression.cpp -o anime ./anime"
+//To run the code use:  "g++ -Iinclude main_LinearRegression.cpp"
 #include <iostream>
 #include <vector>
 // Add the header file for linear regression
